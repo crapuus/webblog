6b9e80f6fb4d9233521ca3f2aa8776f557f07b6b
crapuus <choirulrizqy2017@gmail.com>
2023-05-10 05:22:54 +0700

    membuat fitur log out part 2

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -22,12 +22,23 @@ class Login extends CI_Controller {
 	{
 		$login_salah ='';
 
+		if($this->session->has_userdata('username')){
+			redirect('backend/dashboard');
+		}
+
+		
+
 		if($this->input->post()){
 
 			$username = $this ->input ->post('username');
 			$password = $this ->input ->post('password');
 
-			if($username == 'uus' && $password == '123'){
+			$user = \Orm\User::first(); 
+			if($username == $user->username && $password == $user->password){
+				$userdata = [
+					'username' => $user->username,
+				];
+				$this->session->set_userdata($userdata);
 			redirect('backend/dashboard');
 			} else{
 			$login_salah ='kombinasi username & password salah';
@@ -37,6 +48,13 @@ class Login extends CI_Controller {
 		
 
 		view('login', ['login_salah' => $login_salah]);
+
+	}
+
+	public function logout(){
+
+		$this->session->sess_destroy();
+		redirect('login');
 
 	}
 
